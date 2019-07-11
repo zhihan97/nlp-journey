846c8d68b0e488f3c46f7d435ab56bd31c3e935d
msg901216 <1107306870@qq.com>
2019-07-11 21:36:02 +0800

    gensim lda: 未完成

---
+++ nlp/topic/lda_topic.py
@@ -0,0 +1,36 @@
+from gensim.models import LdaModel
+from gensim.test.utils import common_texts
+from gensim.corpora.dictionary import Dictionary
+import path
+
+
+class LdaTopicModel:
+
+    def __init__(self, model_path,
+                 train=False,
+                 file_path=None):
+        self.model_path = model_path
+        if not train:
+            self.model = self.load_model()
+        else:
+            self.file_path = file_path
+            self.model = self.train()
+
+    def train(self):
+
+        model = LdaModel(self, num_topics=10)
+        return model
+
+    def save_model(self):
+        self.model.save(self.model_path)
+        pass
+
+    def load_model(self):
+        try:
+            model = LdaModel.load(self.model_path)
+        except FileNotFoundError:
+            model = None
+        return model
+
+    def preprocess(self):
+        pass
