c512b574dc3d59c33b82d4a4a4a7d3b18366ac60
msgi <mayuan120226@163.com>
2019-07-21 15:09:46 +0800

    keras tutoral整理

---
+++ tutorials/keras-tutorial/06.load_saved_model.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from keras.models import load_model
+
+# 加载训练好的模型
+model = load_model('./models/trained_model.h5')
+
+X = pd.read_csv("./dataset/proposed_new_product.csv").values
+prediction = model.predict(X)
+
+# 待预测的数据
+prediction = prediction[0][0]
+
+# 重新恢复归一化之前的数值
+prediction = prediction + 0.1159
+prediction = prediction / 0.0000036968
+
+print("Earnings Prediction for Proposed Product : ${}".format(prediction))
+
