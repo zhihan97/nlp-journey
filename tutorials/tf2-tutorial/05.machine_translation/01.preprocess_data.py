60e0c302a6cddc8bba6e3cfbdd7b444e0e98e89e
msgi <mayuan120226@163.com>
2019-11-24 17:03:19 +0800

    preprocess data

--- tutorials/tf2-tutorial/05.machine_translation/01.preprocess_data.py
+++ tutorials/tf2-tutorial/05.machine_translation/01.preprocess_data.py
@@ -3,6 +3,8 @@
 
 import re
 import unicodedata
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
 
 
 # 第一个参数指定字符串标准化的方式。
@@ -42,18 +44,72 @@ def parse_data(filename):
     return zip(*preprocessed_sentence_pairs)
 
 
+# 分词和词语到索引的转换
+def tokenizer(lang):
+    # 用空格分词
+    lang_tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=None, filters='', split=' ')
+    # 先分词
+    lang_tokenizer.fit_on_texts(lang)
+    # 再创建词到索引的映射
+    tensor = lang_tokenizer.texts_to_sequences(lang)
+    # 不够长度的在后边补0
+    tensor = tf.keras.preprocessing.sequence.pad_sequences(tensor, padding='post')
+    return tensor, lang_tokenizer
+
+
+# 获取句子的最大长度
+def max_length(tensor):
+    return max(len(t) for t in tensor)
+
+
+# 拆分训练测试数据集
+def split(input_tensor, output_tensor, test_size=0.2):
+    return train_test_split(input_tensor, output_tensor, test_size=test_size)
+
+
+# 构建训练集
+def make_dataset(input_tensor, output_tensor, batch_size=64, epochs=20, shuffle=True):
+    dataset = tf.data.Dataset.from_tensor_slices((input_tensor, output_tensor))
+    if shuffle:
+        dataset = dataset.shuffle(30000)
+    dataset = dataset.repeat(epochs).batch(batch_size, drop_remainder=True)
+    return dataset
+
+
 if __name__ == '__main__':
     spa_eng_path = './spa-eng/spa.txt'
-    en = 'Come on.'
-    spa = 'Ándale.'
-
-    print(unicode_to_ascii(en))
-    print(unicode_to_ascii(spa))
-
-    print(preprocess_sentence(en))
-    print(preprocess_sentence(spa))
-
     en_dataset, spa_dataset = parse_data(spa_eng_path)
-
     print(en_dataset[-1])
     print(spa_dataset[-1])
+
+    # 西班牙语到英语的训练
+    input_tensor, input_tokenizer = tokenizer(spa_dataset[:30000])
+    output_tensor, output_tokenizer = tokenizer(en_dataset[:30000])
+
+    print(input_tensor)
+    print(output_tensor)
+
+    # 查看输入和输出的最大长度
+    print(max_length(input_tensor))
+    print(max_length(output_tensor))
+
+    # 拆分数据集为训练集和验证集
+    input_train, input_eval, output_train, output_eval = split(input_tensor, output_tensor)
+    print(len(input_train), len(input_eval), len(output_train), len(output_eval))
+
+    # 构建tf.data格式
+    train_dataset = make_dataset(input_train, output_train)
+    eval_dataset = make_dataset(input_eval, output_eval)
+
+    # 打印一下
+    for x, y in train_dataset.take(1):
+        print(x.shape)
+        print(y.shape)
+        print(x)
+        print(y)
+
+
+
+
+
+
